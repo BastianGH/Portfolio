92d244949b91baf7f2a854e29c10a30dbc957198
BastianGH <bastianpussey16@gmail.com>
2024-05-27 16:04:31 +0200

    Initial commit

---
+++ frontend/src/Layout/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+      <header className="App-header">
+        <p>
+         Here is the Header
+        </p>
+      </header>
+  );
+}
+
+export default Header;
