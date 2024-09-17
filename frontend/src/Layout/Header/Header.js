7c84355e37af8fa94cda861008a910c877380071
BastianGH <bastianpussey16@gmail.com>
2024-09-17 16:41:18 +0200

    Correction de style générales

--- frontend/src/Layout/Header/Header.js
+++ frontend/src/Layout/Header/Header.js
@@ -1,29 +1,27 @@
-import './Header.css';
-import logo from './logo.svg'
+import "./Header.css";
+import logo from "./logo.svg";
 
 function Header() {
   return (
-      <header className="header">
-        <a to='/'>
-            <img className="header-img" src={logo} alt="logo"/>
-        </a>        
-        <nav className="header-nav">
-            <ul className="header-nav-list">
-                <li>
-                    <a to='#about'>À Propos de moi</a>
-                </li>
-                <li>
-                    <a to='#experience'>Expérience</a>
-                </li>
-                <li>
-                    <a to='#realisations'>Réalisations</a>
-                </li>
-                <li>
-                    <a to='#tools'>Mes outils</a>
-                </li>
-            </ul>
-        </nav>
-      </header>
+    <header className="header">
+      <a to="/">Bastian PUSSEY</a>
+      <nav className="header-nav">
+        <ul className="header-nav-list">
+          <li>
+            <a to="#about">À Propos de moi</a>
+          </li>
+          <li>
+            <a to="#experience">Expérience</a>
+          </li>
+          <li>
+            <a to="#realisations">Réalisations</a>
+          </li>
+          <li>
+            <a to="#tools">Mes outils</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
