44a49588e1dd1aa9019a02eae47368798718f640
BastianGH <bastianpussey16@gmail.com>
2024-09-20 17:18:01 +0200

    init page réalisations

--- frontend/src/pages/Achievements/Achievements.js
+++ frontend/src/pages/Achievements/Achievements.js
@@ -1,5 +1,22 @@
 import React from "react";
+import Card from "../../components/card/card";
+import data from "../../data/db.json";
 
 export const Achievements = () => {
-  return <div>Here is the Achievements section</div>;
+  console.log(data.achievements);
+  return (
+    <div>
+      <h1>Mes réalisations</h1>
+      <div className="card-container">
+        {data.achievements.map((achievement) => (
+          <Card
+            key={achievement.id}
+            img={achievement.image}
+            title={achievement.title}
+            desc={achievement.short_desc}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
