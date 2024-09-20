c5da15908db31b370f83d285e4dc2d0e2386cf1b
BastianGH <bastianpussey16@gmail.com>
2024-09-20 18:36:00 +0200

    Ajout du filtre

--- frontend/src/pages/Achievements/Achievements.js
+++ frontend/src/pages/Achievements/Achievements.js
@@ -1,15 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import Card from "../../components/card/card";
 import data from "../../data/db.json";
 import "./achievements.css";
 
 export const Achievements = () => {
-  console.log(data.achievements);
+  const [achievements, setAchievements] = useState(data.achievements);
+
+  function filter(e) {
+    if (e.target.value == "") {
+      setAchievements(data.achievements);
+    } else {
+      const filteredArray = data.achievements.filter(
+        (achievement) => achievement.type == e.target.value
+      );
+      setAchievements(filteredArray);
+    }
+  }
+
   return (
     <div>
-      <h1>Mes réalisations</h1>
+      <div className="title-div">
+        <h1>Mes réalisations</h1>
+        <div>
+          <label for="project-select">Filter par : </label>
+          <select
+            name="projects"
+            id="project-select"
+            onChange={(e) => filter(e)}
+          >
+            <option value="">Tous</option>
+            <option value="professional">Projets Professionnels</option>
+            <option value="education">Projets de Formations</option>
+            <option value="personal">Projets Personnels</option>
+          </select>
+        </div>
+      </div>
       <div className="card-container">
-        {data.achievements.map((achievement) => (
+        {achievements.map((achievement) => (
           <Card
             key={achievement.id}
             id={achievement.id}
