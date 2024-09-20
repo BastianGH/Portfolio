a403be336564298f3e0a588985163c07988fe03b
BastianGH <bastianpussey16@gmail.com>
2024-09-20 16:50:48 +0200

    init page about

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,5 +1,66 @@
 import React from "react";
 
 export const About = () => {
-  return <div>Here is the about section</div>;
+  return (
+    <div>
+      <h1>à propos de moi</h1>
+      <section>
+        <div className="xp-content">
+          <img
+            src="./assets/images/photo-BP.jpg"
+            alt="Bastian PUSSEY"
+            className="xp-img"
+          />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            semper consectetur est, eget elementum est rhoncus non. Pellentesque
+            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
+            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
+            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
+            Nulla.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            semper consectetur est, eget elementum est rhoncus non. Pellentesque
+            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
+            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
+            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
+            Nulla.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            semper consectetur est, eget elementum est rhoncus non. Pellentesque
+            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
+            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
+            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
+            Nulla.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            semper consectetur est, eget elementum est rhoncus non. Pellentesque
+            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
+            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
+            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
+            Nulla.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            semper consectetur est, eget elementum est rhoncus non. Pellentesque
+            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
+            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
+            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
+            Nulla.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            semper consectetur est, eget elementum est rhoncus non. Pellentesque
+            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
+            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
+            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
+            Nulla.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
