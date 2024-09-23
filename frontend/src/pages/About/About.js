10c162dfaeb53bff0ba53dd9ff8a70a8ce83108e
BastianGH <bastianpussey16@gmail.com>
2024-09-23 12:34:51 +0200

    Corrections Experience

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -12,52 +12,50 @@ export const About = () => {
             className="xp-img"
           />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Je suis un développeur FullStack avec une solide expérience en
+            alternance, ce qui m’a permis de conjuguer des efforts entre
+            formation et pratique professionnelle. Pendant ma formation chez
+            OpenClassroom, j’ai eu l’opportunité de travailler chez Iris Digital
+            en tant que développeur, où j'ai contribué au développement de sites
+            vitrines tout en avançant sur mes études. Cette expérience m’a
+            appris à gérer efficacement plusieurs responsabilités en parallèle,
+            en étant rigoureux et organisé.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Durant mon passage chez Iris Digital, j’ai principalement utilisé
+            Vue.js dans un environnement Ubuntu pour développer des sites, mais
+            j’ai également pris part à la création d’une API en trois tiers en
+            utilisant NestJS. Ce projet m’a permis de renforcer mes compétences
+            en backend et en conception d’architectures logicielles complexes.
+            Grâce à cela, j’ai acquis une maîtrise solide de la gestion des
+            bases de données et du développement backend "from scratch", tout en
+            étant à l’aise avec Firebase, que j'ai utilisé dans plusieurs
+            projets récents pour la gestion d’authentification et de base de
+            données.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Je suis passionné par le code rigoureux et bien structuré, en accord
+            avec l’importance que j’accorde à l’architecture des projets. Je
+            valorise également la flexibilité, non seulement dans la manière
+            dont je travaille mais aussi dans mes interactions sociales et
+            professionnelles. Autonome et toujours en quête d’apprentissage, je
+            m’adapte rapidement aux nouvelles technologies, notamment en
+            frontend avec Vue.js, React.js et Angular, mais aussi en backend
+            avec NestJS et d’autres solutions sur mesure.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Je me repose sur des outils comme Visual Studio Code pour le
+            développement, GitHub pour la gestion de version, et Trello pour
+            l'organisation. Mon approche du développement est centrée sur la
+            création de solutions claires, lisibles et facilement maintenables,
+            tout en restant ouvert aux retours pour une amélioration continue.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Aujourd’hui, je cherche à poursuivre ma croissance en tant que
+            développeur FullStack, avec un intérêt particulier pour les projets
+            qui demandent une réflexion approfondie sur l’architecture, et où je
+            peux mettre à profit mes compétences techniques variées pour
+            concevoir des applications robustes et évolutives.
           </p>
         </div>
       </section>
--- frontend/src/pages/Experience/Experience.js
+++ frontend/src/pages/Experience/Experience.js
@@ -6,7 +6,11 @@ export const Experience = () => {
     <div>
       <h1>Mon expérience</h1>
       <section>
-        <h2>Iris Digital</h2>
+        <div className="xp-title">
+          <h2>Iris Digital</h2>
+          <span className="poste">Développeur FullStack</span>
+        </div>
+
         <div className="xp-content">
           <img
             src="./assets/images/logo-iris.png"
@@ -14,52 +18,43 @@ export const Experience = () => {
             className="xp-img"
           />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Mon expérience chez Iris Digital, où j’ai travaillé en tant que
+            développeur FullStack en alternance, a été une période marquante
+            dans mon développement professionnel. J'ai su concilier les
+            exigences académiques de ma formation chez OpenClassroom avec les
+            responsabilités professionnelles au sein de la startup, ce qui a
+            renforcé ma capacité à gérer des priorités multiples et à évoluer
+            efficacement dans un environnement dynamique.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Chez Iris Digital, j’ai participé à la conception et au
+            développement de plusieurs sites vitrines, y compris celui de
+            l’entreprise elle-même ainsi qu’un projet pour un client externe. En
+            tant que développeur front-end, j’ai principalement utilisé Vue.js
+            pour créer des interfaces utilisateurs modernes et réactives,
+            travaillant dans un environnement Ubuntu qui m’a permis d’optimiser
+            mon flux de travail avec des outils open source.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            L'un des projets les plus complexes sur lequel j’ai eu la chance de
+            travailler était une architecture en trois tiers, dans laquelle j’ai
+            utilisé NestJS pour le backend et Angular pour le frontend. Ce
+            projet m’a offert une immersion profonde dans le monde du
+            TypeScript, dont la rigidité m’a permis de renforcer la sécurité des
+            applications via un contrôle strict des types et une gestion
+            rigoureuse des requêtes. Cette expérience m’a non seulement permis
+            d’acquérir une bonne maîtrise d’Angular, mais également d’améliorer
+            mes compétences en matière de sécurité applicative, en intégrant des
+            pratiques de surcontrôle des requêtes pour assurer la robustesse de
+            l'API.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            semper consectetur est, eget elementum est rhoncus non. Pellentesque
-            non ante sed ipsum porttitor auctor. Mauris magna ex, porttitor nec
-            sagittis quis, aliquam in elit. Integer sodales neque nibh, ut
-            sodales libero mollis vel. Sed semper ex vitae venenatis iaculis.
-            Nulla.
+            Cette alternance a été pour moi une opportunité unique de renforcer
+            mes compétences techniques en FullStack, tout en apprenant à
+            travailler dans l’environnement exigeant et agile d’une startup. La
+            diversité des projets m’a permis de mieux comprendre les besoins des
+            clients et de proposer des solutions adaptées, tout en respectant
+            les meilleures pratiques de développement.
           </p>
         </div>
       </section>
