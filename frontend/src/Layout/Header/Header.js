9c34ffb338234ec54240f4d64a7df2cc37d14315
BastianGH <bastianpussey16@gmail.com>
2024-10-16 09:09:45 +0200

    Burger menu done

--- frontend/src/Layout/Header/Header.js
+++ frontend/src/Layout/Header/Header.js
@@ -1,7 +1,14 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [burgerState, setBurgerState] = useState(false);
+
+  const burgerMenu = () => {
+    setBurgerState(!burgerState);
+  };
+
   return (
     <header className="header">
       <NavLink to="/">
@@ -20,6 +27,35 @@ function Header() {
           </li>
         </ul>
       </nav>
+      <div className="icon" onClick={burgerMenu}>
+        {!burgerState ? (
+          <div className="burger-icon">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        ) : (
+          <>
+            <div className="cross-icon">
+              <div></div>
+              <div></div>
+            </div>
+            <nav className="burger-menu">
+              <ul className="header-nav-list">
+                <li>
+                  <NavLink to="about">À Propos de moi</NavLink>
+                </li>
+                <li>
+                  <NavLink to="experience">Expérience</NavLink>
+                </li>
+                <li>
+                  <NavLink to="achievements">Réalisations</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </>
+        )}
+      </div>
     </header>
   );
 }
