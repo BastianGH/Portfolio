2649472db8968e234980aeb159457082f176b7d2
BastianGH <bastianpussey16@gmail.com>
2024-10-18 09:04:31 +0200

    finition menu burger

--- frontend/src/Layout/Header/Header.js
+++ frontend/src/Layout/Header/Header.js
@@ -10,53 +10,60 @@ function Header() {
   };
 
   return (
-    <header className="header">
-      <NavLink to="/">
-        <span className="title-name">Bastian PUSSEY</span>
-      </NavLink>
-      <nav className="header-nav">
-        <ul className="header-nav-list">
-          <li>
-            <NavLink to="about">À Propos de moi</NavLink>
-          </li>
-          <li>
-            <NavLink to="experience">Expérience</NavLink>
-          </li>
-          <li>
-            <NavLink to="achievements">Réalisations</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className="icon" onClick={burgerMenu}>
-        {!burgerState ? (
-          <div className="burger-icon">
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        ) : (
-          <>
-            <div className="cross-icon">
+    <>
+      <header className="header">
+        <NavLink to="/">
+          <span className="title-name">Bastian PUSSEY</span>
+          <span className="title-abbreviated">BP</span>
+        </NavLink>
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li>
+              <NavLink to="about">À Propos de moi</NavLink>
+            </li>
+            <li>
+              <NavLink to="experience">Expérience</NavLink>
+            </li>
+            <li>
+              <NavLink to="achievements">Réalisations</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="icon" onClick={burgerMenu}>
+          {!burgerState ? (
+            <div className="burger-icon">
+              <div></div>
               <div></div>
               <div></div>
             </div>
-            <nav className="burger-menu">
-              <ul className="header-nav-list">
-                <li>
-                  <NavLink to="about">À Propos de moi</NavLink>
-                </li>
-                <li>
-                  <NavLink to="experience">Expérience</NavLink>
-                </li>
-                <li>
-                  <NavLink to="achievements">Réalisations</NavLink>
-                </li>
-              </ul>
-            </nav>
-          </>
-        )}
-      </div>
-    </header>
+          ) : (
+            <>
+              <div className="cross-icon">
+                <div></div>
+                <div></div>
+              </div>
+            </>
+          )}
+        </div>
+      </header>
+      {burgerState ? (
+        <nav className="burger-menu">
+          <ul className="header-nav-list">
+            <li>
+              <NavLink to="about">À Propos de moi</NavLink>
+            </li>
+            <li>
+              <NavLink to="experience">Expérience</NavLink>
+            </li>
+            <li>
+              <NavLink to="achievements">Réalisations</NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
